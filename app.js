841190bf1bcf0acbbc8201d8753b348a98a76cb7
Holi0317 <holliswuhollis@gmail.com>
2015-07-26 19:48:10 +0800

    Various changes to API. Making it more awesome ;)

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/v1', apiRouter);
 app.use('/', routes);
+app.use('/api/v1/mc-mods', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,43 +1,60 @@
 var express = require('express');
+var soundex = require('soundex-encode');
+var fuse = require('fuse.js');
 var router = express.Router();
 var models = require('../models');
-var soundex = require('soundex-encode');
 
 Array.prototype.contains = function(element){
   return this.indexOf(element) > -1;
 };
 
-router.get('/', function (req, res, next) {
-  res.send();
-});
-
-// Last update
-router.get('/info', function (req, res, next) {
+router.use(function (req, res, next) {
   models.info
-    .findOne({})
-    .select('lastUpdate')
-    .exec(function (err, data) {
-      if (err) {
-        res.status(500).json({'message': 'Query error. See server log for details'});
-        console.error('Error when querying tags. Error: ', err);
-        return;
-      }
-      res.json(data);
-    });
+  .findOne({})
+  .select('lastUpdate')
+  .exec(function (err, data) {
+    if (err) {
+      res.status(500).json({'message': 'Query error. See server log for details'});
+      console.error('Error when querying tags. Error: ', err);
+      return;
+    }
+    res.set('Last-Modified', data.lastUpdate.toISOString());
+    next();
+  });
 });
 
-// List and search cached mods
-router.get('/list', function (req, res, next) {
+router.head('/', function (req, res, next) {
+  models['mc-mods']
+  .count({})
+  .exec(function (err, count) {
+    if (err) {
+      res.status(500);
+      console.error('Error when counting. Error: ', err);
+      return;
+    }
+    res.set('X-length', count);
+    res.send('');
+  });
+});
+
+router.get('/', function (req, res, next) {
+  var queryParm = {};
+  var sortMapper = {
+    fancyname: 'fancyName',
+    id: 'id_',
+    author: 'author',
+    downloadcount: 'downloadCount',
+    score: 'score'  // Would not sort anything in database
+  };
+
   var parm = {
     page: (req.query.page) ? Number(req.query.page): 1,
     sort: (req.query.sort) ? String(req.query.sort): 'downloadCount',
     limit: (req.query.limit === 'all') ? Number.MAX_SAFE_INTEGER-1: req.query.limit,
     search: (req.query.search) ?  req.query.search: '',
-    fuzzy: (req.query.fuzzy) ? true: false
+    soundex: (req.query.soundex) ? true: false,
+    sortBy: {}
   };
-  var accepedSort = ['fancyName', 'slug', 'id_', 'description', 'author', 'downloadCount', 'tags'];
-  var queryParm;
-  var sortBy = {};
 
   // Section: Validicate
   // Validicate limit parm
@@ -56,11 +73,13 @@ router.get('/list', function (req, res, next) {
   }
 
   // Validicate sort parameter
-  if (!accepedSort.contains(parm.sort)) {
+  if (typeof sortMapper[parm.sort.toLowerCase()] === 'undefined') {
     res.status(400).json({
       'message': 'Incorrect sort parameter.'
     });
     return;
+  } else {
+    parm.sort = sortMapper[parm.sort.toLowerCase()];
   }
 
   // Validicate page
@@ -73,36 +92,40 @@ router.get('/list', function (req, res, next) {
 
   // Query parm
   if (!parm.search) {
-    queryParm = {};
-  } else if (parm.fuzzy) {
-    queryParm = {
-        fancyNameSoundex: soundex(parm.search)
-    };
+    // No-op
+  } else if (parm.soundex) {
+    queryParm.fancyNameSoundex = soundex(parm.search);
   } else {
-    queryParm = {
-      fancyNameFlatten: {
-        $regex: new RegExp(parm.search.replace(/[^a-zA-Z0-9]/g, '').toLowerCase())
-      }
+    queryParm.fancyNameFlatten = {
+      $regex: new RegExp(parm.search.replace(/[^a-zA-Z0-9]/g, '').toLowerCase())
     };
   }
 
-  sortBy[parm.sort] = -1;
+  parm.sortBy[parm.sort] = -1;
 
   // Section: query and return
   models['mc-mods'].paginate(queryParm, {
     page: parm.page,
     limit: parm.limit,
-    sortBy: {
-      downloadCount: -1
-    },
-    // populate: ['id', 'slug', 'fancyName', 'fancyNameSoundex', 'description', 'author', 'downloadCount', 'tags']
+    sortBy: parm.sortBy,
+    columns: '-_id id_ slug fancyName fancyNameSoundex description author downloadCount tags',
   }, function (err, results, pageCount, itemCount) {
     if (err) {
       console.error('Error when querying mc-mods. Error: ', err);
       res.status(500).json({'message': 'Query error. See server log for details'});
       return;
     }
-    res.json(results);
+    res.set({
+      'X-page-count': pageCount,
+      'X-length': itemCount
+    });
+    if (parm.sort !== 'sort') res.json(results);
+    else {
+      res.json(new fuse(results, {
+        keys: ["fancyName"],
+        threshold: 1,
+      }).search(parm.search));
+    }
   });
 
 });
@@ -110,16 +133,16 @@ router.get('/list', function (req, res, next) {
 // List all tags
 router.get('/tags', function (req, res, next) {
   models.info
-    .findOne({})
-    .select('tags')
-    .exec(function (err, data) {
-      if (err) {
-        res.status(500).json({'message': 'Query error. See server log for details'});
-        console.error('Error when querying tags. Error: ', err);
-        return;
-      }
-      res.json(data);
-    });
+  .findOne({})
+  .select('tags')
+  .exec(function (err, data) {
+    if (err) {
+      res.status(500).json({'message': 'Query error. See server log for details'});
+      console.error('Error when querying tags. Error: ', err);
+      return;
+    }
+    res.json(data.tags);
+  });
 });
 
 
