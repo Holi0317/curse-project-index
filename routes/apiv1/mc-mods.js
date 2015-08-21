6788d28e78b792ffb8f93b01d36463e37c003e02
Holi0317 <holliswuhollis@gmail.com>
2015-08-21 11:30:21 +0800

    Separated query parse to a function
    This makes it more modular, for better maintenance

--- routes/apiv1/mc-mods.js
+++ routes/apiv1/mc-mods.js
@@ -1,14 +1,97 @@
+"use strict";
+
 var express = require('express');
 var Fuse = require('fuse.js');
 var async = require('async');
 var router = express.Router();
 var models = require('../../models');
 
-Array.prototype.contains = function(element){
-  return this.indexOf(element) > -1;
-};
+function deserializeQuery(query) {
+  /*
+  * Deserialize query string
+  * @param (Object) query: Raw query object from client
+
+  * @return (Object): deserialized query object, with following keys
+  * (Object) find - JSON doc for querying mongodb
+  * (String) fuseSearch - String to be searched by fuse.js
+  * (Number) threshold - threshold for fuse.js
+  * (String) sort - mongodb query parameter
+  * (Number) limit - mongodb query parameter
+  * (Bool) needFuse - Require fuse.js to search?
+
+  * @throw (String): Error in parameter. Response it with HTTP status 400 and in json
+  */
+  var sortTarget;
+  var sortMapper = {
+    fancyname: 'fancyName',
+    id: 'id_',
+    author: 'author',
+    downloadcount: '-downloadCount',
+    score: 'score'  // Would not sort anything in database level
+  };
+
+  var result = {
+    find: {},
+    fuseSearch: (query.search) ? query.search: '',
+    threshold: 0.6,
+    sort: '-downloadCount',
+    limit: 10,
+    needFuse: false,
+  };
+
+  // Section: Validicate
+  // Validicate limit parm
+
+  if (typeof query.limit === 'undefined') {
+    // Remains default, 10
+  } else if (query.limit.toLowerCase() === 'all') {
+    result.limit = Number.MAX_SAFE_INTEGER-1;
+  } else if (query.limit < 1) {
+    throw 'How can I give you less than 1 data?';
+  } else if (query.limit > Number.MAX_SAFE_INTEGER-1) {
+    throw 'Limit parameter cannot be larger than 9007199254740990';
+  } else if (!isNaN(query.limit)) {
+    result.limit = Number(query.limit);
+  } else {
+    throw 'Incorrect limit parameter. Only "all", undefined and Number larger than 0 is accepted';
+  }
+
+  // Validicate sort parameter
+  if (typeof query.sort !== 'undefined') {
+    sortTarget = sortMapper[query.sort.toLowerCase()];
+    if (typeof sortTarget === 'undefined') {
+      throw 'Incorrect sort parameter.';
+    } else {
+      result.sort = sortTarget;
+    }
+  }
+
+  // Query parameter
+  if (!query.search) {
+    // Does not need to search
+  } else if (query.search.length > 32) {
+    throw 'Search string length must be less than 32. #Blame fuse.js';
+  } else {
+    // Requires fuse to search
+    result.needFuse = true;
+    result.find.fancyName = new RegExp('^'+query.search[0], 'i');
+    result.limit = Number.MAX_SAFE_INTEGER-1;
+  }
+
+  // threshold
+  if (typeof query.threshold === 'undefined') {
+    // Remains default
+  } else if (isNaN(query.threshold) || query.threshold < 0 || query.threshold > 1) {
+    throw 'Threshold should be an number between 0 and 1';
+  } else {
+    result.threshold = Number(query.threshold);
+  }
+
+  return result;
+}
 
 router.use(function (req, res, next) {
+  // Inject header Last-Modified to response
   models.info
   .findOne({})
   .select('lastUpdate')
@@ -42,107 +125,48 @@ router.get('/', function (req, res, next) {
   * The master plan: validicate and format params. -> Query and sort from mongo.
   * -> Create fuse object. -> search and return.
   */
-  var sortMapper = {
-    fancyname: 'fancyName',
-    id: 'id_',
-    author: 'author',
-    downloadcount: '-downloadCount',
-    score: 'score'  // Would not sort anything in database
-  };
+  var query;
 
-  var parm = {
-    sort: (req.query.sort) ? req.query.sort: 'downloadCount',
-    limit: (req.query.limit === 'all') ? Number.MAX_SAFE_INTEGER-1: req.query.limit,
-    search: (req.query.search) ? req.query.search: '',
-    threshold: (req.query.threshold) ? Number(req.query.threshold): 0.6,
-  };
-  var needFuse;
-
-  // Section: Validicate
-  // Validicate limit parm
-  if (!parm.limit) {
-    // undefined
-    parm.limit = 10;
-  } else if (isNaN(parm.limit)) {
-    // Not a number nor 'all'
-    res.status(400).json({
-      'message': 'Incorrect limit parameter. Only "all", undefined and Number larger than 0 is accepted'
+  try {
+    query = deserializeQuery(req.query);
+  } catch (err) {
+    return res.status(400).json({
+      message: err
     });
-    return;
-  } else if (parm.limit < 1) {
-    // Less than 1. Kidding?
-    res.status(400).json({
-      'message': 'How can I give you less than 1 data?'
-    });
-    return;
-  }
-
-  // Validicate sort parameter
-  if (typeof sortMapper[parm.sort.toLowerCase()] === 'undefined') {
-    // Key not in sortMapper. Incorrect parameter
-    res.status(400).json({
-      'message': 'Incorrect sort parameter.'
-    });
-    return;
-  } else {
-    parm.sort = sortMapper[parm.sort.toLowerCase()];
-  }
-
-  // Query parm
-  if (!parm.search) {
-    // Undefined
-    needFuse = false;
-  } else if (parm.search > 32) {
-    res.status(400).json({
-      'message': 'Search string length must be less than 32. #Blame fuse.js'
-    });
-    return;
-  } else {
-    // Requires fuse to search
-    needFuse = true;
-    parm.limit = Number.MAX_SAFE_INTEGER-1;
-  }
-
-  // threshold
-  if (isNaN(parm.threshold) || parm.threshold < 0 || parm.threshold > 1) {
-    res.status(400).json({
-      'message': 'Threshold should be an number between 0 and 1'
-    });
-    return;
   }
 
   // Section: query and return
-  models['mc-mods']
-  .find({})
-  .sort(parm.sort)
-  .limit(parm.limit)
-  .select('-_id id_ slug fancyName description author downloadCount tags')
-  .exec(function (err, results) {
-    if (err) {
-      console.error('Error when querying mc-mods. Error: ', err);
-      res.status(500).json({'message': 'Query error. See server log for details'});
-      return;
-    }
+  try {
+    models['mc-mods']
+    .find(query.find)
+    .sort(query.sort)
+    .limit(query.limit)
+    .select('-_id id_ slug fancyName description author downloadCount tags')
+    .exec(function (err, results) {
+      if (err) throw err;
 
-    if (needFuse) {
-      var fuse = new Fuse(results, {
-        keys: ['fancyName'],
-        threshold: parm.threshold,
-        shouldSort: parm.sort === 'score',
-      });
-      var fuseAsync = async.asyncify(fuse.search.bind(fuse));
-      fuseAsync(parm.search, function (error, result) {
-        if (error) {
-          console.error(error);
-          res.status(500).json({'message': 'Query error. See server log for details'});
-          return;
-        }
-        res.json(result);
-      });
-    } else {
-      res.json(results);
-    }
-  });
+      if (query.needFuse) {
+        var fuse = new Fuse(results, {
+          keys: ['fancyName'],
+          threshold: query.threshold,
+          shouldSort: query.sort === 'score',
+        });
+        var fuseAsync = async.asyncify(fuse.search.bind(fuse));
+        fuseAsync(query.fuseSearch, function (error, result) {
+          if (err) throw err;
+          return res.json(result);
+        });
+      } else {
+        return res.json(results);
+      }
+    });
+  } catch (err) {
+    console.error('Error when querying database. Error: ', err);
+    res.status(500).json({
+      message: 'Query error. Check server log for details'
+    });
+    return;
+  }
 
 });
 
